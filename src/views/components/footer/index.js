f355aee7f8aecbccf141efacb61986ca115bfddc
llfalcao <luizotavio-falcao@hotmail.com>
2021-07-20 20:51:22 -0300

    Add blur background and fix responsive design

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -1,7 +1,7 @@
 import planet from "../../../assets/images/elements/planet.png";
 
 let footer = `
-    <div class="container footer-contain">
+    <div id="about" class="container footer-contain">
         <p>
             Star Wars - este é um exemplo criado para os Alunos Gama Academy
             Módulo de boas práticas.
--- src/views/components/sections/HomeSectionOne/index.js
+++ src/views/components/sections/HomeSectionOne/index.js
@@ -1,11 +1,13 @@
-import logo from "../../../assets/images/logo/logo.png";
-import backgroundOne from "../../../assets/images/background/bg-1.jpg";
+import logo from "../../../../assets/images/logo/logo.png";
+import backgroundOne from "../../../../assets/images/background/bg-1.jpg";
 
 let homeSectionOne = `
     <section id="section-one" class="section background--one" style="background-image: url(${backgroundOne})">
         <div class="container block-section--one">
             <h1>CLIQUE NO BABY YODA, ELE TE MOSTRARÁ O CAMINHO</h1>
-            <img src="${logo}" alt="Star Wars Logo" width="300px" height="auto">
+            <a href="#about">
+                <img src="${logo}" alt="Star Wars Logo" width="300px" height="auto">
+            </a>
         </div>
     </section>
 `;
